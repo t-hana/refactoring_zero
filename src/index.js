c81444449f71801073f1093b36b4d3f6a242a54b
花房智明 <hanafusa.tomoaki@a-tm.co.jp>
2023-08-23 14:32:11 +0900

    初期状態

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import play from "../json/plays.json" assert { type: "json" };
-import nvoices from "../json/nvoices.json" assert { type: "json" };
-import { statement } from "./statement.js";
-
-console.log(statement(nvoices, play));
